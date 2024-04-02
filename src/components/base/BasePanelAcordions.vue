43fdeb5719eabbc21a7f6562ddd4ca049168b2c2
Martynav.cniitu <martynav.cniitu@gmail.com>
2024-04-02 17:58:00 +0300

    new layout

--- src/components/base/BasePanelAcordions.vue
+++ src/components/base/BasePanelAcordions.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-expansion-panels style="gap: 1px" class="base" variant="popout" >
+  <v-expansion-panels style="gap: 1px" class="base" variant="popout">
     <v-expansion-panel
       v-for="item in panel"
       :id="item"
@@ -7,18 +7,18 @@
       :elevation="3"
       :title="item.title"
       color="#2c4957"
-      :hide-actions="!item.panels"
+      :hide-actions="!item.items"
       bg-color="#ebebeb"
       @click="toId(item.title)"
     >
-      <v-expansion-panel-text v-if="item.panels"  class="elevetion-0">
+      <v-expansion-panel-text v-if="item.items" class="elevetion-0">
         <v-btn
-          v-for="itm in item.panels"
+          v-for="itm in item.items"
           :key="itm"
           class="menu-button rounded-0"
           block
-          @click="toId(itm)"
-          >{{ itm }}</v-btn
+          @click="to(itm)"
+          >{{ itm.title }}</v-btn
         >
       </v-expansion-panel-text>
     </v-expansion-panel>
@@ -26,10 +26,12 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, defineEmits } from 'vue'
+import BaseAutocomplite from './BaseAutocomplite.vue';
+const emit = defineEmits(['update:tab'])
 const props = defineProps({
   elevation: { type: String, default: '5' }, //тень
-  propsPanel: { type: Array }, //1-панель будет открыта при отрисовке
+  propsPanel: { type: Array, default: [] }, //1-панель будет открыта при отрисовке
   bgColor: { type: String, default: '#fff3e4' }, // фон панели заголовка
   tColor: { type: String, default: '#546e7a' } // цвет панели заголовка
 })
@@ -38,6 +40,10 @@ panel.value = props.propsPanel
 function toId(id) {
   let elem = document.getElementById(id)
   elem.scrollIntoView({ behavior: 'smooth', block: 'center' })
+}
+function to(item) {
+  console.log('item', item.title)
+  emit('update:tab', item.title)
 }
 </script>
 
@@ -48,5 +54,9 @@ function toId(id) {
 ::v-deep .v-expansion-panel-title--active {
   font-weight: bolder;
   font-size: 18px;
+}
+::v-deep .v-btn__content {
+  white-space: pre-wrap;
+  padding: 10px 5px;
 }
 </style>
--- src/components/layout/LayoutPanelTabs.vue
+++ src/components/layout/LayoutPanelTabs.vue
@@ -7,7 +7,11 @@
       @click="isOpen = true"
     ></v-btn>
     <div class="panel-menu">
-      <BasePanelAcordions :props-panel="data"></BasePanelAcordions>
+      <BasePanelAcordions
+        :props-panel="data"
+        :tab="tab"
+        @update:tab="tab = $event"
+      ></BasePanelAcordions>
       <v-btn @click="testGreen">TEST green</v-btn>
       <v-btn @click="testRed">TEST red</v-btn>
     </div>
@@ -21,37 +25,20 @@
         props-panel="1"
         bg-color="#ebebeb"
       >
-        <template #title>Panel Title {{ item.title }}</template>
-        <div v-if="item.panels" class="tabsWrapper">
-          <v-tabs v-model="model" align-tabs="right">
-            <v-tab v-for="itm in item.panels" :key="itm" :value="itm"> {{ itm }} </v-tab>
+        <template #title>{{ item.title }} </template>
+        <div v-if="item.items" class="tabsWrapper">
+          <v-tabs v-model="tab" align-tabs="right" density="compact">
+            <v-tab v-for="itm in item.items" :key="itm" :value="itm.title"> {{ itm.title }} </v-tab>
           </v-tabs>
 
-          <v-window v-model="model">
-            <v-window-item v-for="itm in item.panels" :id="itm" :key="itm" :value="itm">
-              <p>{{ text }}</p>
+          <v-window v-model="tab">
+            <v-window-item v-for="itm in item.items" :id="itm" :key="itm" :value="itm.title">
+              <p style="padding: 20px">{{ text }}</p>
             </v-window-item>
           </v-window>
-
-          <!-- <base-panel
-              v-for="itm in item.panels"
-              :id="itm"
-              :key="itm"
-              elevation="2"
-              props-panel="1"
-              color="#2c4957"
-              bg-color="#dbdbdb"
-            >
-              <template #title>Title Children</template>
-              <h1>{{ itm }}</h1>
-            </base-panel> -->
         </div>
         <div v-else style="padding: 20px">
-          <h1>BasePanel</h1>
-          <h1>BasePanel</h1>
-          <h1>BasePanel</h1>
-          <h1>BasePanel</h1>
-          <h1>BasePanel</h1>
+          <p style="padding: 20px">{{ text }}</p>
         </div>
       </base-panel>
     </div>
@@ -62,20 +49,38 @@
 import BasePanel from '../base/BasePanel.vue'
 import MenuRegistration from '../MenuRegistration.vue'
 import BasePanelAcordions from '../base/BasePanelAcordions.vue'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import { useSnackStore } from '@/stores/snackStore'
 const snack = useSnackStore()
 const isOpen = ref(false)
-const data = [
-  { panels: ['children11', 'children12', 'children13'], title: 'панель1' },
-  { title: 'панель2' },
-  { panels: ['children31', 'children32', 'children33', 'children34'], title: 'панель3' },
-  { panels: ['children41', 'children42'], title: 'панель4' },
-  { title: 'панель5' }
-]
-const model = ref('tab-2')
-const text =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+const tab = ref('панель1')
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
 
 function testGreen() {
   snack.setSnack({ text: 'Просто какой-то Текст ', type: 'info' })
@@ -83,210 +88,187 @@ function testGreen() {
 function testRed() {
   snack.setSnack({ text: 'Ошибка загрузки', type: 'error' })
 }
-
-
-// items0() {
-//       return [
-//         {
-//           title: 'Документ об оценке соответствия',
-//           id: '#vehicle-details',
-//           items: [
-//             {
-//               title: 'Документ',
-//               id: '#vehicle-details-document',
-//             },
-//             {
-//               title: 'Транспортное средство',
-//               id: '#vehicle-details-vehicle',
-//             },
-//             {
-//               title: 'Наименование органа, выдавшего документ',
-//               id: '#vehicle-details-certification',
-//             },
-//             {
-//               title: 'Заявитель и его адрес',
-//               id: '#vehicle-details-applicant',
-//             },
-//             {
-//               title: 'Изготовитель и его адрес',
-//               id: '#vehicle-details-manufacturer',
-//             },
-//             {
-//               title: 'Представители изготовителя и их адрес',
-//               id: '#vehicle-details-representatives',
-//             },
-//             {
-//               title: 'Сборочный завод и его адрес',
-//               id: '#vehicle-details-factory',
-//             },
-//             {
-//               title: 'Поставщик сборочных комплектов и его адрес',
-//               id: '#vehicle-details-provider',
-//             },
-//             {
-//               title: 'Вид распространения',
-//               id: '#vehicle-details-view',
-//             },
-//             {
-//               title: 'Дополнительная информаци',
-//               id: '#vehicle-details-additional',
-//             },
-//           ],
-//         },
-//       ];
-//     },
-//     items() {
-//       return [
-//         // {
-//         //   title: 'Документ об оценке соответствия',
-//         //   id: '#vehicle-details',
-//         // },
-//         {
-//           title: 'Базовое ТС',
-//           id: '#base-vehicle',
-//         },
-//       ];
-//     },
-//     items2() {
-//       return [
-//         {
-//           title: 'Общие характеристики транспортного средства (Шасси)',
-//           items: [
-//             {
-//               title: 'Компоновка транспортного средства',
-//               id: '#vehicle-composition',
-//             },
-//             {
-//               title: 'Ходовая часть ТС',
-//               id: '#numbers-vheels',
-//             },
-//             {
-//               title: 'Ось транспортного средства',
-//               id: '#vehicle-axis',
-//             },
-//             {
-//               title: 'Габаритные размеры',
-//               id: '#dimensions',
-//             },
-//             {
-//               title: 'Масса',
-//               id: '#weight',
-//             },
-//             {
-//               title: 'Масса буксируемого прицепа',
-//               id: '#weight-towed-trailer',
-//             },
-//             {
-//               title: 'Двигатель',
-//               id: '#engine',
-//               hide: this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)),
-//             },
-//             {
-//               title: 'Устройство накопления энергии',
-//               id: '#storage-device',
-//               hide: this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)),
-//             },
-//             {
-//               title: 'Топливо',
-//               id: '#fuel',
-//               hide:
-//                 this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
-//                 this.engineType === 'Двигатель внутреннего сгорания',
-//             },
-//             {
-//               title: 'Система питания',
-//               id: '#supply-system',
-//               hide:
-//                 this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
-//                 this.engineType === 'Двигатель внутреннего сгорания',
-//             },
-//             {
-//               title: 'Система зажигания',
-//               id: '#ignition-system',
-//               hide:
-//                 this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
-//                 this.engineType === 'Двигатель внутреннего сгорания',
-//             },
-//             {
-//               title: 'Система выпуска и нейтрализации отработавших газов',
-//               id: '#neutralization-system',
-//               hide:
-//                 this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
-//                 this.engineType === 'Двигатель внутреннего сгорания',
-//             },
-//             {
-//               title: 'Сцепление',
-//               id: '#clutch',
-//               hide: this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)),
-//             },
-//             {
-//               title: 'Трансмиссия',
-//               id: '#transmission',
-//               hide: this.techCategoryCode.every(i => !['O1', 'O2', 'O3', 'O4'].includes(i)),
-//             },
-//             /*  {
-//               title: "Коробка передач",
-//               id:"#gear-box"
-//             }, */
-//             {
-//               title: 'Подвеска',
-//               id: '#suspension',
-//             },
-//             {
-//               title: 'Рулевое управление',
-//               id: '#steerage',
-//             },
-//             {
-//               title: 'Тормозные системы',
-//               id: '#brake-systems',
-//             },
-//             {
-//               title: 'Шины',
-//               id: '#tires',
-//             },
-//           ],
-//         },
-//       ];
-//     },
-//     items3() {
-//       return [
-//         {
-//           title: 'Описание маркировки транспортного средства (Шасси)',
-//           id: '#description-of-vehicle-markings',
-//         },
-//         {
-//           title: 'Общий вид транспортного средства (Шасси)',
-//           id: '#vehicle-general-form',
-//         },
-//         {
-//           title: 'Документ, подтверждающий соответствие обязательным требованиям',
-//           id: '#confirming-document',
-//         },
-//         {
-//           title: 'Перечень документов, являющихся основанием для оформления ОТТС',
-//           id: '#list-of-documents',
-//           hide: this.formModel.conformityDocKindCode === '30',
-//         },
-//         {
-//           title: 'Перечень документов, являющихся основанием для оформления ОТШ',
-//           id: '#list-of-documents',
-//           hide: this.formModel.conformityDocKindCode === '35',
-//         },
-//         {
-//           title: 'История изменения документа',
-//           id: '#conformity-change-history',
-//         },
-//       ];
-//     },
+const data = [
+  {
+    title: 'Документ об оценке соответствия',
+    id: '#vehicle-details',
+    items: [
+      {
+        title: 'Документ',
+        id: '#vehicle-details-document'
+      },
+      {
+        title: 'Транспортное средство',
+        id: '#vehicle-details-vehicle'
+      },
+      {
+        title: 'Наименование органа, выдавшего документ',
+        id: '#vehicle-details-certification'
+      },
+      {
+        title: 'Заявитель и его адрес',
+        id: '#vehicle-details-applicant'
+      },
+      {
+        title: 'Изготовитель и его адрес',
+        id: '#vehicle-details-manufacturer'
+      },
+      {
+        title: 'Представители изготовителя и их адрес',
+        id: '#vehicle-details-representatives'
+      },
+      {
+        title: 'Сборочный завод и его адрес',
+        id: '#vehicle-details-factory'
+      },
+      {
+        title: 'Поставщик сборочных комплектов и его адрес',
+        id: '#vehicle-details-provider'
+      },
+      {
+        title: 'Вид распространения',
+        id: '#vehicle-details-view'
+      },
+      {
+        title: 'Дополнительная информаци',
+        id: '#vehicle-details-additional'
+      }
+    ]
+  },
+  {
+    title: 'Базовое ТС',
+    id: '#base-vehicle'
+  },
+  {
+    title: 'Общие характеристики транспортного средства (Шасси)',
+    items: [
+      {
+        title: 'Компоновка транспортного средства',
+        id: '#vehicle-composition'
+      },
+      {
+        title: 'Ходовая часть ТС',
+        id: '#numbers-vheels'
+      },
+      {
+        title: 'Ось транспортного средства',
+        id: '#vehicle-axis'
+      },
+      {
+        title: 'Габаритные размеры',
+        id: '#dimensions'
+      },
+      {
+        title: 'Масса',
+        id: '#weight'
+      },
+      {
+        title: 'Масса буксируемого прицепа',
+        id: '#weight-towed-trailer'
+      },
+      {
+        title: 'Двигатель',
+        id: '#engine'
+        // hide: this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i))
+      },
+      {
+        title: 'Устройство накопления энергии',
+        id: '#storage-device'
+        // hide: this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i))
+      },
+      {
+        title: 'Топливо',
+        id: '#fuel'
+        // hide:
+        //   this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
+        //   this.engineType === 'Двигатель внутреннего сгорания'
+      },
+      {
+        title: 'Система питания',
+        id: '#supply-system'
+        // hide:
+        //   this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
+        //   this.engineType === 'Двигатель внутреннего сгорания'
+      },
+      {
+        title: 'Система зажигания',
+        id: '#ignition-system'
+        // hide:
+        //   this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
+        //   this.engineType === 'Двигатель внутреннего сгорания'
+      },
+      {
+        title: 'Система выпуска и нейтрализации отработавших газов',
+        id: '#neutralization-system'
+        // hide:
+        //   this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i)) &&
+        //   this.engineType === 'Двигатель внутреннего сгорания'
+      },
+      {
+        title: 'Сцепление',
+        id: '#clutch'
+        // hide: this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i))
+      },
+      {
+        title: 'Трансмиссия',
+        id: '#transmission'
+        // hide: this.techCategoryCode.every((i) => !['O1', 'O2', 'O3', 'O4'].includes(i))
+      },
+      {
+        title: 'Подвеска',
+        id: '#suspension'
+      },
+      {
+        title: 'Рулевое управление',
+        id: '#steerage'
+      },
+      {
+        title: 'Тормозные системы',
+        id: '#brake-systems'
+      },
+      {
+        title: 'Шины',
+        id: '#tires'
+      }
+    ]
+  },
+  {
+    title: 'Описание маркировки транспортного средства (Шасси)',
+    id: '#description-of-vehicle-markings'
+  },
+  {
+    title: 'Общий вид транспортного средства (Шасси)',
+    id: '#vehicle-general-form'
+  },
+  {
+    title: 'Документ, подтверждающий соответствие обязательным требованиям',
+    id: '#confirming-document'
+  },
+  {
+    title: 'Перечень документов, являющихся основанием для оформления ОТТС',
+    id: '#list-of-documents'
+    // hide: this.formModel.conformityDocKindCode === '30'
+  },
+  {
+    title: 'Перечень документов, являющихся основанием для оформления ОТШ',
+    id: '#list-of-documents'
+    // hide: this.formModel.conformityDocKindCode === '35'
+  },
+  {
+    title: 'История изменения документа',
+    id: '#conformity-change-history'
+  }
+]
 </script>
 
 <style scoped>
+::v-deep .v-btn__content {
+  white-space: pre-wrap;
+}
 .tabsWrapper {
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
   gap: 12px;
-  max-width: 50vw;
   overflow: auto;
 }
 .panel-layot {
